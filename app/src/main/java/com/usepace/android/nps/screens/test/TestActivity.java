e2000647a059a978be2e0603415527397fd19ea2
Mohamed <mohhamednabil@gmail.com>
2019-02-20 15:25:37 +0300

    UI

--- app/src/main/java/com/usepace/android/nps/screens/test/TestActivity.java
+++ app/src/main/java/com/usepace/android/nps/screens/test/TestActivity.java
@@ -1,14 +1,17 @@
 package com.usepace.android.nps.screens.test;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import com.usepace.android.nps.R;
+import com.usepace.android.nps.screens.rating.RatingActivity;
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        startActivity(new Intent(this, RatingActivity.class));
     }
 }
