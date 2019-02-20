ce03327e7d45641a8e976ea68bb6f56940222644
Mohamed <mohhamednabil@gmail.com>
2019-02-20 10:56:54 +0300

    Sdk init

---
+++ app/src/main/java/com/usepace/android/nps/screens/test/TestActivity.java
@@ -0,0 +1,14 @@
+package com.usepace.android.nps.screens.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.usepace.android.nps.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
